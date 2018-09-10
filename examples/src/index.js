d070b60e4ed0b29c0b63987eb5fc9c7115a7e139
Adam Anderson <adam.m.anderson@gmail.com>
2018-09-10 16:22:34 +0100

    wrapper component for example

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Chatty from '../../src';
+import React from 'react'
+import { render } from 'react-dom'
+import Chatty from '../../src'
+import styled from 'react-emotion'
+import { injectGlobal } from 'emotion'
+
+injectGlobal`
+  body, html, {
+    min-height: 100%;
+    margin: 0;
+  }
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 650px;
+    height: 100%;
+  }
+`
+
+const Wrapper = styled.div`
+  height: 600px;
+  width: 400px;
+  background: #efefef;
+`;
 
 const App = () => (
+  <Wrapper>
     <Chatty />
-);
-render(<App />, document.getElementById("root"));
+  </Wrapper>
+)
+
+render(<App />, document.getElementById("root"))
